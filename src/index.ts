05c1bdd96bed15f1f42c4a1b20e6d0ac043aec1e
Bivek <beevk.singh@gmail.com>
2021-08-04 01:49:36 +0700

    feat: add db connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 import { authRoutes } from "./routes";
 import errorHandler from "./middlewares/errorHandler";
@@ -18,6 +19,23 @@ app.all('*', async () => {
 })
 
 app.use(errorHandler);
-app.listen(3000, () => {
-    console.log('Auth server started on port: 3000');
-})
+
+const startServer = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.info('Connected to DB!');
+    } catch (e) {
+        console.error('Connecting to DB failed!', e);
+    }
+
+    app.listen(3000, () => {
+        console.log('Auth server started on port: 3000');
+    });
+}
+
+startServer();
+
