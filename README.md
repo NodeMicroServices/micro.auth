3bea07c7be1f9d66b0f93f4666ee5836d5499378
Bivek <beevk.singh@gmail.com>
2021-08-30 01:30:53 +0700

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# micro.auth
+# Authentication - Node Microservice
 Micro Service for Authentication using NodeJS, TypeScript &amp; MongoDB
+
+## Important
+Make sure you've ```mongodb-memory-server``` installed globally in your system for testing purposes.
+
+```bash
+#Application Setup
+$ npm i
+
+# This starts the app in development mode
+$ npm run start
+
+# Pre-requisite for test
+$ npm i -g mongodb-memory-server
+# Testing
+$ npm run test
+```
+
+## License
+
+---
+This app is released under the [MIT license](https://opensource.org/licenses/MIT).
+
+
+
+
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import morgan from 'morgan';
 import cookieSession from "cookie-session";
+import cors from 'cors';
 
 import { authRoutes } from "./routes";
 import errorHandler from "./middlewares/errorHandler";
@@ -18,7 +19,12 @@ app.use(morgan('tiny'));
 // Add secure: true => to enable sharing cookie only through HTTPS
 app.use(cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: false //process.env.NODE_ENV !== 'test'
+}));
+
+//Change this later
+app.use(cors({
+    origin: 'http://local.host'
 }));
 
 app.use('/users', authRoutes);
